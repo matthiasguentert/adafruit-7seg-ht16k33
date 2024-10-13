66a5c215e50b52473ece9121202d27098713b61f
matthiasguentert <matthias.guentert@gmail.com>
2024-10-13 22:31:33 +0200

    adds basic examples

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # adafruit-7seg-ht16k33
+
+Basic examples for the 0.56" 7-segment LED HT16K33 Backpack working with a Feather 32u4
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,48 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_7segment matrix = Adafruit_7segment();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  matrix.begin(0x70);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // printing a number that's to long results in ----
+  // matrix.print(10000, DEC);
+  // matrix.writeDisplay();
+  // delay(500);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // print a hex number
+  // matrix.print(0xBEEF, HEX);
+  // matrix.writeDisplay();
+  // delay(500);
+
+  // print a floating point
+  // matrix.print(12.34);
+  // matrix.writeDisplay();
+  // delay(500);
+
+  // print time with colon
+  // matrix.print(1234);
+  // matrix.drawColon(true);
+  // matrix.writeDisplay();
+  // delay(10000);
+
+  // print a string message
+  // matrix.print("7SEG");
+  // matrix.writeDisplay();
+  // delay(10000);
+
+  // draws "2.345"
+  // matrix.blinkRate(HT16K33_BLINK_1HZ); // start blinking
+  // matrix.writeDigitNum(0, 2, true);
+  // matrix.writeDigitNum(1, 3);
+  // matrix.writeDigitNum(3, 4);
+  // matrix.writeDigitNum(4, 5);
+  // matrix.writeDisplay();
+  // delay(5000);
+  // matrix.blinkRate(HT16K33_BLINK_OFF); // stop blinking
+  // delay(5000);
 }
